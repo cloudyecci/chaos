e1484c77b7c45c5392277163cef8b8c2670a9fc4
yangxiao <3088943730@qq.com>
2024-08-28 21:47:50 +0800

    display time
    显示时间

--- src/main/java/com/tii/HelloWorld.java
+++ src/main/java/com/tii/HelloWorld.java
@@ -2,12 +2,15 @@ package com.tii;
 
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(isPalindrome("123321"));
-        System.out.println(LocalDateTime.now());
+        //按照给定的格式显示当前时间
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        System.out.println(LocalDateTime.now().format(dtf));
     }
     //判断一个字符串是否是回文字符串
     public static boolean isPalindrome(String str){
