8e482b466a390b57db1652d17934b55fbc588c4f
yangxiao <3088943730@qq.com>
2024-09-01 11:21:53 +0800

    display time
    显示时间

--- src/main/java/com/tii/HelloWorld.java
+++ src/main/java/com/tii/HelloWorld.java
@@ -11,6 +11,8 @@ public class HelloWorld {
         //按照给定的格式显示当前时间
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         System.out.println(LocalDateTime.now().format(dtf));
+        int l = length(8, 3);
+        System.out.println(l);
     }
     //判断一个字符串是否是回文字符串
     public static boolean isPalindrome(String str){
@@ -27,5 +29,14 @@ public class HelloWorld {
             right--;
         }
         return true;
+    }
+
+    public static int length(int h, int n){
+        int l = 0;
+        for(int i = 1; i<= n; i++){
+            l += h;
+            h /= 2;
+        }
+        return l;
     }
 }
